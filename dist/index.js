562955fbc375115650f2da751b167118a5e9e114
Rob Porter <rob@weeverapps.com>
2018-06-12 12:09:53 -0400

    Fix storage thing

--- dist/index.js
+++ dist/index.js
@@ -14,26 +14,24 @@ var _logger = require('./lib/logger');
 
 var _logger2 = _interopRequireDefault(_logger);
 
-var _nodePersist = require('node-persist');
-
-var _nodePersist2 = _interopRequireDefault(_nodePersist);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var storage = require('node-persist');
 
 require('dotenv').config();
 
-_nodePersist2.default.initSync();
+storage.initSync();
 
 var doCheck = function doCheck() {
-    return (0, _compare.check)(_nodePersist2.default.getItemSync('browsers')).then(function (newBrowsers) {
-        _nodePersist2.default.setItemSync('browsers', newBrowsers);
+    return (0, _compare.check)(storage.getItemSync('browsers')).then(function (newBrowsers) {
+        storage.setItemSync('browsers', newBrowsers);
     });
 };
 
 // @todo switch to cache via storage like songster does for settings
 // init
 _browsers2.default.get().then(function (resp) {
-    _nodePersist2.default.setItemSync('browsers', resp);
+    storage.setItemSync('browsers', resp);
     _nodeCron2.default.schedule('0 * * * *', doCheck);
 
     // first time run through.
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import cron from 'node-cron';
 import { check } from './lib/compare';
 import browsers from './lib/browsers';
 import logger from './lib/logger';
-import storage from 'node-persist';
+
+const storage = require('node-persist');
 
 require('dotenv').config();
 
